adc274ea78121d2dad2f7775da8c1f97ac0c380f
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2020-05-21 15:44:46 +0100

    updated MongoDB schema, title not mandatory

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ mongoose.connect("mongodb://localhost:27017/notesDB", {
 });
 
 const noteSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "title is required as part of the schema."],
-  },
+  title: String,
   content: String,
 });
 
